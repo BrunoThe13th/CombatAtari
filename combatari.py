d9885f145b3cef94a57eefb6ba4bd4db5385a95b
lmc2631 <lmc.snf19@uea.edu.br>
2019-10-13 20:15:45 -0400

    Colisão do player com as paredes (ainda sem o mapa)
    Colisão do player com as paredes (ainda sem o mapa)

--- combatari.py
+++ combatari.py
@@ -1,11 +1,13 @@
 import turtle
 
+# Criando Tela
 screen = turtle.Screen()
 screen.title("Combat_atari")
 screen.bgcolor("green")
 screen.setup(720,720)
 screen.tracer(0)
 
+# Criando jogador 1
 player_1 = turtle.Turtle("turtle")
 player_1.speed(0)
 player_1.turtlesize(1.5,1.5)
@@ -13,6 +15,7 @@ player_1.color("blue")
 player_1.penup()
 player_1.setx(330)
 
+# Criando jogador 2
 player_2 = turtle.Turtle("turtle")
 player_2.speed(0)
 player_2.turtlesize(1.5,1.5)
@@ -20,14 +23,17 @@ player_2.color("red")
 player_2.penup()
 player_2.setx(-330)
 
+# Rotação dos jogadores para a direita
 def player1_movement_right():
     player_1.right(30)
 def player2_movement_right():
     player_2.right(30)
+# Rotação dos jogadores para a esquerda
 def player1_movement_left():
     player_1.left(30)
 def player2_movement_left():
     player_2.left(30)
+# Movimentação dos jogadores para a frente
 def player1_movement_forward():
     player_1.forward(15)
 def player2_movement_forward():
@@ -46,3 +52,23 @@ screen.listen()
 while True:
     screen.update()
     screen.update()
+    # Limitações da tela no eixo X
+    if player_1.xcor() > 335:
+        player_1.setx(335)
+    if player_1.xcor() < -335:
+        player_1.setx(-335)
+    if player_2.xcor() > 335:
+        player_2.setx(335)
+    if player_2.xcor() < -335:
+        player_2.setx(-335)
+    # Limitações da tela no eixo Y
+    if player_1.ycor() > 335:
+        player_1.sety(335)
+    if player_1.ycor() < -335:
+        player_1.sety(-335)
+    if player_2.ycor() > 335:
+        player_2.sety(335)
+    if player_2.ycor() < -335:
+        player_2.sety(-335)
+
+    
