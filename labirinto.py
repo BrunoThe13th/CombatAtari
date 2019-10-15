d4f16a848b5c69913efb6ce1d5ff361e9e329a02
ndmf.snf19@uea.edu.br <ndmf.snf19@uea.edu.br>
2019-10-14 23:08:09 -0400

    labirinto quase pronto

--- labirinto.py
+++ labirinto.py
@@ -1,23 +1,23 @@
 import turtle
 import random
 
-file = open('labirinto.txt', 'r')
-wall = file.readlines()
-
 
 # Funçao que gera as paredes
 def generate_walls(x, y):
+    file = open('labirinto.txt', 'r')
+    wall = file.readlines()
     posy_block = 200
     for lines in range(x):
+        wall1 = wall[lines].strip().split(",")
         posx_block = -353
         for columns in range(y):
-            block = turtle.Turtle("square")
-            block.speed(0)
-            block.color("")
-            block.shapesize(0.5, 0.5)
-            block.penup()
-            block.goto(posx_block, posy_block)
+            if (wall1[columns] == '1'):
+                block = turtle.Turtle("square")
+                block.speed(0)
+                block.color("white")
+                block.shapesize(0.5, 0.5)
+                block.penup()
+                block.goto(posx_block, posy_block)
             posx_block += 10
         posy_block -= 10
-
-file.close()
+    file.close()
